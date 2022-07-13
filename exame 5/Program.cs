b6ed6f2c6838a32bbd162793979029925eda6948
Diego Moraes dos Santos Silva <diego.moraes@cadmus.com.br>
2022-07-12 21:59:40 -0300

    Adicionar arquivos de projeto.

---
+++ exame 5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exame_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, num;
+
+            for (i = 0; i < 100; i++)
+
+            {
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("Numero par: " + i);
+                }
+            }
+        }
+    
+    }
+}
